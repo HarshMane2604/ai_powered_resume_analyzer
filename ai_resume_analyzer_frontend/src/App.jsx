d4815deeecaaf6d2fedecbb216290eb3ae6dc2fb
Your Name <maneharsh72@gmail.com>
2025-11-29 00:38:17 +0530

    Added some dependency and pages

--- ai_resume_analyzer_frontend/src/App.jsx
+++ ai_resume_analyzer_frontend/src/App.jsx
@@ -2,19 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './styles/index.css'
-import UploadBox from './components/UploadBox'
+
 import Router from './routes/router.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    
     <>
-      <h1 class="text-3xl font-bold underline text-amber-700">
-        AI Resume Analyzer
-      </h1>
-      <UploadBox onFileSelect={(file) => console.log(file)} />
       <Router />
     </>
   )
