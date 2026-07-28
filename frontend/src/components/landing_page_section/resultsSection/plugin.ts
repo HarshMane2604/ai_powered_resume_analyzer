f357a40d66eac54eff2cce9cd316003bf10e54dd
Harsh Mane <maneharsh72@Gmail.com>
2026-07-28 16:11:12 +0530

    Implementing CI-CD pipeline

--- frontend/src/components/landing_page_section/resultsSection/plugin.ts
+++ frontend/src/components/landing_page_section/resultsSection/plugin.ts
@@ -3,7 +3,6 @@ import ResultsSection from "./ResultsSection";
 
 export const resultsSectionPlugin: LandingPlugin = {
     id: "resultsSection",
-    name: "Results Section",
     enabled: true,
     order: 3, // Header is likely 1, Feature is 2
     component: ResultsSection
