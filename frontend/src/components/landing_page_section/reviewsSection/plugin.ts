8426d92d566b0d6ba57d9107252e2a14ed47dcec
Harsh Mane <maneharsh72@Gmail.com>
2026-07-28 16:12:42 +0530

    Implementing CI-CD pipeline

--- frontend/src/components/landing_page_section/reviewsSection/plugin.ts
+++ frontend/src/components/landing_page_section/reviewsSection/plugin.ts
@@ -3,7 +3,7 @@ import ReviewsSection from "./ReviewsSection";
 
 export const reviewsSectionPlugin: LandingPlugin = {
     id: "reviewsSection",
-    name: "Reviews Section",
+    
     enabled: true,
     order: 4,
     component: ReviewsSection
